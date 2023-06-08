dfefae6efd208d524703bde7c51df94845b32059
Nathan Reents <130861854+NJRFirstOfHisName@users.noreply.github.com>
2023-06-08 11:27:53 -0600

    Adjust random color generation to occur once per square

--- script.js
+++ script.js
@@ -26,7 +26,9 @@ function colorPixels() {
     const allPixels = document.querySelectorAll("div.pixel");
     allPixels.forEach((pixel) => {
         pixel.addEventListener('mouseover', () => {
-            pixel.style.backgroundColor = getRandomColor();
+            if (!pixel.style.backgroundColor) {
+                pixel.style.backgroundColor = getRandomColor();
+            }
         })
     })
 }
@@ -48,4 +50,9 @@ function getRandomColor() {
       color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
-  }
+}
+
+const RGB_Linear_Shade=(p,c)=>{
+    var i=parseInt,r=Math.round,[a,b,c,d]=c.split(","),P=p<0,t=P?0:255*p,P=P?1+p:1-p;
+    return"rgb"+(d?"a(":"(")+r(i(a[3]=="a"?a.slice(5):a.slice(4))*P+t)+","+r(i(b)*P+t)+","+r(i(c)*P+t)+(d?","+d:")");
+}
